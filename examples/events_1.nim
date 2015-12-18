c616e5ae5cb08bc53c4be0e5c6cedcbd845815e8
Andreas Rumpf <rumpf_a@web.de>
2015-12-18 23:46:41 +0100

    reworked event handling to support Nim's closures

--- examples/events_1.nim
+++ examples/events_1.nim
@@ -6,9 +6,10 @@ var timecounter = 0
 var timerWx: WxTimer = nil
 var timerEx: WxTimerEx = nil
 
-proc timerExEvent(fun: pointer, parent: WxWindow, evn: WxTimerEvent) =
+proc timerExEvent(evn: WxEvent) {.nimcall.} =
   if evn == nil:
     return
+  let evn = WxTimerEvent(evn)
 
   inc timecounter
   echo "Its about time #", timecounter, " (interval is ", evn.getInterval, " milliseconds)"
@@ -24,12 +25,13 @@ proc timerExEvent(fun: pointer, parent: WxWindow, evn: WxTimerEvent) =
     timerEx.delete()
     echo "timerEx stopped"
 
-proc timerWxEvent (fun: pointer, parent: WxWindow, evn: WxTimerEvent) =
+proc timerWxEvent(evn: WxEvent) {.nimcall.} =
   if evn == nil:
     return
-  echo "One shot TimerWx Boom!"    
+  echo "One shot TimerWx Boom!"
 
-proc keyPressed(fun: pointer, parent: WxWindow, evn: WxKeyEvent) =
+proc keyPressed(evn: WxEvent) {.nimcall.} =
+  let evn = WxKeyEvent(evn)
   if evn == nil:
     return
   echo "keyPressed (on Button)..."
@@ -38,13 +40,13 @@ proc keyPressed(fun: pointer, parent: WxWindow, evn: WxKeyEvent) =
   echo "id: ", evn.getId
   echo "KeyCode: ", wxKeyEvent_GetKeyCode(evn)
   echo "Modifiers: ", wxKeyEvent_GetModifiers(evn)
-  
+
   if wxKeyEvent_GetKeyCode(evn)==67 and wxKeyEvent_GetModifiers(evn)==16:
     # ctrl+c :)
     eljExitMainLoop()
 
 
-proc buttonPressed(fun: pointer, parent: WxWindow, evn: WxEvent) =
+proc buttonPressed(evn: WxEvent) {.nimcall.} =
   if evn == nil:
     return
   echo "buttonPressed (on mainFrame)..."
@@ -53,7 +55,7 @@ proc buttonPressed(fun: pointer, parent: WxWindow, evn: WxEvent) =
   echo "id: ", evn.getId
   evn.skip() # makes that the other one still gets called!
 
-proc buttonQuit(fun: pointer, parent: WxWindow, evn: WxEvent) =
+proc buttonQuit(evn: WxEvent) {.nimcall.} =
   if evn == nil:
     return
 
@@ -65,7 +67,37 @@ proc buttonQuit(fun: pointer, parent: WxWindow, evn: WxEvent) =
   evn.skip() # makes that the other one still gets called!
   eljExitMainLoop()
 
-proc appMain(argc: pointer, argv: openArray[cstring]) =
+type
+  EventHandler = proc (event: WxEvent) {.closure.}
+  YetAnotherClosure = ref object
+    eventHandler: EventHandler
+
+proc rawEventHandler(fun, data, event: pointer) {.cdecl.} =
+  let d = cast[YetAnotherClosure](data).eventHandler
+  d(cast[WxEvent](event))
+
+proc register(obj: WxWindow; kind: int;
+              eventHandler: EventHandler) =
+  let data = YetAnotherClosure(eventHandler: eventHandler)
+  # we leak the environment here. This seems to be the best we
+  # can do since wxC doesn't offer the possibility to override
+  # the destructor of wxClosure in wrapper.h.
+  GC_ref(data)
+  discard obj.connect(-1, -1, kind, wxClosure(rawEventHandler, cast[pointer](data)))
+
+proc register(obj: WxButton; eventHandler: EventHandler) =
+  register(obj, expEVT_COMMAND_BUTTON_CLICKED(), eventHandler)
+
+proc register(obj: WxTimerEx; eventHandler: EventHandler) =
+  let data = YetAnotherClosure(eventHandler: eventHandler)
+  # we leak the environment here. This seems to be the best we
+  # can do since wxC doesn't offer the possibility to override
+  # the destructor of wxClosure in wrapper.h.
+  GC_ref(data)
+  obj.wxTimerEx_Connect(wxClosure(rawEventHandler, cast[pointer](data)))
+
+
+proc appMain(a, b, c: pointer) {.cdecl.} =
   # argc und argv do not make sense to me :(
 
   let mainFrame = wxFrame(nil, wxID_ANY, "Hi!", -1, -1, -1, -1, wxDEFAULT_FRAME_STYLE)
@@ -74,23 +106,25 @@ proc appMain(argc: pointer, argv: openArray[cstring]) =
   let sizer = wxBoxSizer(wxVertical)
   let button = wxButton(mainFrame, -12, "Quit", 0,0, -1,-1, 0)
 
-  discard button.connect(-1, -1, expEVT_COMMAND_BUTTON_CLICKED(), wxClosure(buttonPressed, mainFrame))
-  discard mainFrame.connect(-1, -1, expEVT_COMMAND_BUTTON_CLICKED(), wxClosure(buttonQuit, mainFrame))
-  discard button.connect(-1, -1, expEVT_KEY_UP(), wxClosure(keyPressed, mainFrame))
+  button.register(buttonPressed)
+  mainFrame.register(expEVT_COMMAND_BUTTON_CLICKED(), buttonQuit)
+
+  let panel = wxPanel(mainFrame, wxID_ANY)
+  panel.register(expEVT_KEY_UP(), keyPressed)
 
   mainFrame.setSizer(sizer)
-  sizer.addWindow(button, 0, wxALL , 10, nil)
+  sizer.addWindow(button, 0, wxALL, 10, nil)
 
   # new timer owned by mainFrame
   timerWx = wxTimer(mainFrame, wxID_ANY)
   # add event listener for timer events to mainFrame
-  discard mainFrame.connect(-1, -1, expEVT_TIMER(), wxClosure(timerWxEvent, mainFrame))
+  mainFrame.register(expEVT_TIMER(), timerWxEvent)
   # start the timer as one shot after 5 seconds
   discard timerWx.start(5000, true)
 
   # this is just "a timer" with a callback not bound to anything really
   timerEx = wxTimerEx()
-  timerEx.connect(wxClosure(timerExEvent, mainFrame))
+  timerEx.register(timerExEvent)
   discard timerEx.start(1000, false)
 
   mainFrame.fit
