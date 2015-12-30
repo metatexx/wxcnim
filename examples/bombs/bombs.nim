7606e48b80adc9ee9683eb892a436ee561300656
Hans Raaf <hara@oderwat.de>
2015-12-30 03:03:54 +0100

    Using closures for everything now.
    Needs the new LL compiler!

--- examples/bombs/bombs.nim
+++ examples/bombs/bombs.nim
@@ -5,9 +5,6 @@
 # Todos:
 #  Fix first click behavior (no bomb!)
 #  Make different level (sizes)
-#  Add the graphics
-#  Add sound
-#  Better data structure!
 
 import wxcnim
 import mxstring # managed WxString
@@ -31,73 +28,61 @@ type
 
   CellKind = tuple[flags: set[CellBits], count: byte]
 
-type
-  FieldObj = object
-    level: int
-    w, h: int
-    normalCells, bombs: int
-    fieldData: seq[CellKind]
-  Field = ref FieldObj
-
-proc cell(fd: Field, x, y: int): var CellKind =
-  fd.fieldData[y * fd.w + x]
-
-proc createField(w,h : int): Field =
-  result = new FieldObj
-  result.level = 1
-  result.normalCells = 0
-  result.bombs = 0
-  result.w = w
-  result.h = h
-
-  var fd = newSeq[CellKind](w * h)
-  #fd.shallow() # xxx will araq freak out?
-  #result.fieldData = fd
-  shallowCopy(result.fieldData, fd)
-
-  for i in countdown(w * h-1, 0):
-    fd[i].flags = if random(15) < 3: { ckHIDDEN, ckBOMB } else: { ckHIDDEN }
-
-  result.normalCells = 0;
-  for x in 0 ..< w:
-    for y in 0 ..< h:
-      if ckBOMB notin result.cell(x, y).flags:
-        inc result.normalCells
-        for xx in -1 .. 1:
-          for yy in -1 .. 1:
-            if (xx != 0 or yy != 0) and
-              x + xx >= 0 and
-              x + xx < w and
-              y + yy >= 0 and
-              y + yy < h and
-              ckBOMB in result.cell(x + xx, y + yy).flags:
-                inc fd[y * w + x].count
-
-  result.bombs = w * h - result.normalCells;
-
-proc dump(f: Field) =
-  for y in 0 ..< f.w:
-    var row = ""
-    for x in 0 ..< f.h:
-      if ckBOMB in f.cell(x, y).flags:
-        row &= "*"
-      else:
-        row &= $f.cell(x, y).count
-    echo row
-
-proc updateStatus(status: WxStatusBar, f: Field) =
-  # xxx triggered closure bug if used inside appMain
-  #let status = mainFrame.getStatusBar
-  status.setStatusText("Bombs: " & $f.bombs, 0)
-  status.setStatusText("Cells: " & $f.normalCells, 1)
-  status.setStatusText("Level: " & $f.level, 2)
-
 proc appMain() =
   wxnInitAllImageHandlers()
 
-  var f = createField(10,10)
+  var
+    level: int = 1
+    fw, fh: int
+    normalCells, bombs: int
+    fd: seq[CellKind]
 
-  dump f
+  proc cell(x, y: int): var CellKind =
+    fd[y * fw + x]
+
+  proc dump() =
+    for y in 0 ..< fw:
+      var row = ""
+      for x in 0 ..< fh:
+        if ckBOMB in cell(x, y).flags:
+          row &= "*"
+        else:
+          row &= $cell(x, y).count
+      echo row
+
+  fw = 10
+  fh = 10
+
+  proc createField() =
+    level = 1
+    normalCells = 0
+    bombs = 0
+
+    fd = newSeq[CellKind](fw * fh)
+
+    for i in countdown(fw * fh-1, 0):
+      fd[i].flags = if random(15) < 3: { ckHIDDEN, ckBOMB } else: { ckHIDDEN }
+
+    normalCells = 0;
+    for x in 0 ..< fw:
+      for y in 0 ..< fh:
+        if ckBOMB notin cell(x, y).flags:
+          inc normalCells
+          for xx in -1 .. 1:
+            for yy in -1 .. 1:
+              if (xx != 0 or yy != 0) and
+                x + xx >= 0 and
+                x + xx < fw and
+                y + yy >= 0 and
+                y + yy < fh and
+                ckBOMB in cell(x + xx, y + yy).flags:
+                  inc fd[y * fw + x].count
+
+    bombs = fw * fh - normalCells;
+
+  createField()
+
+  dump()
 
   let mainFrame = wxFrame(title="Nim Bombs!",
     stl = wxDEFAULT_DIALOG_STYLE or wxMINIMIZE_BOX)
@@ -113,40 +98,42 @@ proc appMain() =
 
   mainFrame.connect(expEVT_COMMAND_BUTTON_CLICKED()) do(evn: WxEvent):
     #wxnExitMainLoop()
-    f = createField(10, 10)
+    createField()
     #mainFrame.setSize(0, 0, 600, 600, wxSIZE_AUTO)
     panel.fit()
     panel.refresh()
 
-  let status = mainFrame.createStatusBar(3, wxSTB_SIZEGRIP)
-  #mainFrame.wxFrame_SetStatusBar(status)
-  #var fields = [100,-1]
-  #wxFrame_SetStatusWidths(status, 2, addr(fields[0]))
+  let status = mainFrame.createStatusBar(3)
 
-  updateStatus(status, f)
+  proc updateStatus() =
+    status.setStatusText("Bombs: " & $bombs, 0)
+    status.setStatusText("Cells: " & $normalCells, 1)
+    status.setStatusText("Level: " & $level, 2)
+
+  updateStatus()
 
   mainFrame.setSizer(sizer)
-  panel = wxPanel(mainFrame, wxID_ANY, 0,0, (f.w * UNIT), f.h * UNIT, wxBORDER_NONE)
+  panel = wxPanel(mainFrame, wxID_ANY, 0,0, (fw * UNIT), fh * UNIT, wxBORDER_NONE)
 
   sizer.addWindow(panel, 1, wxALL, 10)
   sizer.addWindow(button, 0, wxALL, 10)
 
   proc uncoverAll() =
-    for f in mitems(f.fieldData):
+    for f in mitems(fd):
       f.flags.excl(ckHIDDEN)
     panel.refresh()
 
   proc uncover(x, y: int) =
-    if x >= 0 and x < f.w and y >= 0 and y < f.h:
-      var (flags, count) = f.cell(x, y)
+    if x >= 0 and x < fw and y >= 0 and y < fh:
+      var (flags, count) = cell(x, y)
       # not marked? and hidden?
       if ckMARK notin flags and ckHIDDEN in flags:
         # wft.. boom!
         if ckBOMB in flags:
           echo "BOOM"
-          f.cell(x, y).flags.incl( ckEXPLODED )
-          f.normalCells = 0
-          updateStatus(status, f)
+          cell(x, y).flags.incl( ckEXPLODED )
+          normalCells = 0
+          updateStatus()
           uncoverAll()
           wxnBell() # whatever sound it makes
           return
@@ -154,7 +141,7 @@ proc appMain() =
         # uncover field
         flags.excl( ckHIDDEN )
         # update cell
-        f.cell(x, y).flags = flags
+        cell(x, y).flags = flags
 
         if flags == {} and count == 0:
             for xx in -1 .. 1:
@@ -162,9 +149,9 @@ proc appMain() =
                 if xx != 0 or yy != 0:
                   uncover(x + xx, y + yy)
 
-        dec f.normal_cells
-        updateStatus(status, f)
-        if f.normal_cells == 0:
+        dec normal_cells
+        updateStatus()
+        if normal_cells == 0:
           uncoverAll()
           let msgDlg = wxMessageDialog_Create(
             panel,
@@ -172,33 +159,36 @@ proc appMain() =
             "wxNimBombs",
             wxOK)# or wxICON_INFORMATION)
           discard msgDlg.showModal()
-          f = createField(10, 10)
+          createField()
           panel.refresh()
 
+  # uncover a field
   panel.connect(expEVT_LEFT_DOWN()) do(evn: WxEvent):
     let evn = WxMouseEvent(evn)
-    if f.normalCells == 0:
+    if normalCells == 0:
       return
     let x = evn.getX() div UNIT
     let y = evn.getY() div UNIT
     uncover(x,y)
     panel.refresh()
 
+  # add a "Marker"
   panel.connect(expEVT_RIGHT_DOWN()) do(evn: WxEvent):
     let evn = WxMouseEvent(evn)
-    if f.normalCells == 0:
+    if normalCells == 0:
       return
     let x = evn.getX() div UNIT
     let y = evn.getY() div UNIT
 
-    let flags = f.cell(x, y).flags
+    let flags = cell(x, y).flags
     if ckHIDDEN in flags:
       if ckMARK in flags:
-        f.cell(x, y).flags.excl(ckMARK)
+        cell(x, y).flags.excl(ckMARK)
       else:
-        f.cell(x, y).flags.incl(ckMARK)
+        cell(x, y).flags.incl(ckMARK)
       panel.refresh()
 
+  ## Paint the playfield
   panel.connect(expEVT_PAINT()) do(evn: WxEvent):
     let brushGrey {.global.} = wxBrush(wxColourRGB(150,150,150))
     #let brushGrey = wxGreyBrush()
@@ -211,9 +201,9 @@ proc appMain() =
     #echo validColourName("RAINBOW") #is false :)
     #echo validColourName("MEDIUM FOREST GREEN") #is true :)
 
-    for x in 0 ..< f.w:
-      for y in 0 ..< f.h:
-        let (flags, count) = f.cell(x, y)
+    for x in 0 ..< fw:
+      for y in 0 ..< fh:
+        let (flags, count) = cell(x, y)
 
         if (ckHIDDEN in flags) or (ckMARK in flags):
           dc.setBrush(brushGrey)
